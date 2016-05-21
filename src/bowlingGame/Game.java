bbdfb06783cab2adf72f14b2b27bc8a976a92d16
LiXuFei <xfli@thoughtworks.com>
2016-05-22 01:46:45 +0800

    improve score method and please considering comments part.

--- src/bowlingGame/Game.java
+++ src/bowlingGame/Game.java
@@ -4,22 +4,47 @@ public class Game {
     private int itsScore = 0;
     private int[] itsThrows = new int[21];
     private int currentThrows = 0;
+    private int itsCurrentFrame = 1;
+    private Boolean fistThrow = true;
 
     public void add(int pins) {
         itsThrows[currentThrows++] = pins;
         itsScore += pins;
+        adjustCurrentFrame();
     }
 
     public int score() {
-        return itsScore;
+        return scoreForFrame(getCurrentFrame() - 1);
     }
 
     public int scoreForFrame(int theFrame) {
         int ball = 0;
         int score = 0;
         for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-            score = itsThrows[ball++] + itsThrows[ball++];
+            int firstThrow = itsThrows[ball++];
+            int secondThrow = itsThrows[ball++];
+            int frameScore = firstThrow + secondThrow;
+
+            if (frameScore == 10) {
+                score = frameScore + itsThrows[ball]; //思考为什么不是score = frameScore + itsThrows[ball++];
+            } else {
+                score += frameScore;
+            }
         }
         return score;
+    }
+
+    public int adjustCurrentFrame() {
+        if (fistThrow) {
+            fistThrow = false;
+        } else {
+            fistThrow = true;
+            itsCurrentFrame ++;
+        }
+        return itsCurrentFrame;
+    }
+
+    public int getCurrentFrame() {
+        return itsCurrentFrame;
     }
 }
--- test/bowlingGame/BowlingGameTest.java
+++ test/bowlingGame/BowlingGameTest.java
@@ -30,6 +30,36 @@ public class BowlingGameTest {
 
         assertEquals(17, game.score());
         assertEquals(8, game.scoreForFrame(1));
-        assertEquals(9, game.scoreForFrame(2));
+        assertEquals(17, game.scoreForFrame(2));
+    }
+
+    @Test
+    public void testSimpleFrameAfterSpare () throws Exception {
+        game.add (4);
+        game.add(6);
+        game.add (3);
+        game.add(2);
+
+        assertEquals(18, game.score());
+        assertEquals(13, game.scoreForFrame(1));
+        assertEquals(18, game.scoreForFrame(2));
+    }
+
+//    @Test
+//    public void testOneThrows () throws Exception {
+//        game.add (4);
+//
+//        assertEquals(4, game.score());
+//        assertEquals(4, game.scoreForFrame(1));
+//        assertEquals(1, game.getCurrentFrame());
+//    }
+
+    @Test
+    public void testTwoThrows () throws Exception {
+        game.add (4);
+        game.add (5);
+
+        assertEquals(9, game.scoreForFrame(1));
+        assertEquals(2, game.getCurrentFrame());
     }
 }
